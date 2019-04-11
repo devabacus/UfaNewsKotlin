8f020daaa900add85aa5744e46fca0cb640f3ed1
devabacusnrf <devabacusnrf@gmail.com>
2019-04-11 09:12:43 +0500

    add item view

---
+++ app/src/main/java/com/example/ufanews/list/NewsAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.ufanews.list
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter : RecyclerView.Adapter<NewsViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/example/ufanews/list/NewsViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.ufanews.list
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
