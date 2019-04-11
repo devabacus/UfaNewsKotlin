f09fbee549efeac12e168863a01e86124eb2d28f
devabacusnrf <devabacusnrf@gmail.com>
2019-04-11 10:37:02 +0500

    adapter worked

--- app/src/main/java/com/example/ufanews/list/ListViewFragment.kt
+++ app/src/main/java/com/example/ufanews/list/ListViewFragment.kt
@@ -30,7 +30,18 @@ class ListViewFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ListViewViewModel::class.java)
         rec_view.layoutManager = LinearLayoutManager(context)
-        rec_view.adapter = NewsAdapter()
+        val adapter = NewsAdapter()
+        rec_view.adapter = adapter
+
+        val list : MutableList<News> = mutableListOf()
+        list.add(News("title_news1", "description_news1", "http://www.bashinform.ru/upload/img_res1280/ec70f5a6b43bcd52/f_1_jpg_crop1554900657_ejw_1280.jpg"))
+        list.add(News("title_news2", "description_news2", "http://www.bashinform.ru/upload/img_res1014/57cf7777c2590986/sledkom_jpg_crop1552325137_ejw_1017_jpg_crop1554904872_ejw_1014.jpg"))
+        list.add(News("title_news3", "description_news3", "http://www.bashinform.ru/upload/img_res1280/5e699a8fb5e443fe/YROV5338_2_jpg_crop1547205705_ejw_1280_jpg_crop1554903171_ejw_1280.jpg"))
+        list.add(News("title_news4", "description_news4", "http://www.bashinform.ru/upload/img_res1280/d16d72fdfa1e950c/NkRcouKHtsk_jpg_crop1554899778_ejw_1280.jpg"))
+        list.add(News("title_news5", "description_news5", "http://www.bashinform.ru/upload/img_res1280/6997437d272bd84f/YROV8100_JPG_crop1554703689_ejw_1280_jpg_crop1554898311_ejw_1280.jpg"))
+
+        adapter.setList(list)
+
     }
 
 }
--- app/src/main/java/com/example/ufanews/list/News.kt
+++ app/src/main/java/com/example/ufanews/list/News.kt
@@ -3,5 +3,5 @@ package com.example.ufanews.list
 data class News (
     val title: String,
     val description: String,
-    val imageLink: String
+    val imageLink: String?
 )
