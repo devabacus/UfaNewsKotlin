a1ceb8e0459ed719bae06eb61e469e3a232a7942
devabacusnrf <devabacusnrf@gmail.com>
2019-04-11 19:35:59 +0500

    list of news done

--- app/src/main/java/com/example/ufanews/list/ListViewFragment.kt
+++ app/src/main/java/com/example/ufanews/list/ListViewFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.ufanews.R
 import kotlinx.android.synthetic.main.list_view_fragment.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
@@ -18,12 +19,9 @@ import org.jsoup.Jsoup
 class ListViewFragment : Fragment() {
 
     private val TAG = "myLogs"
-
-    companion object {
-        fun newInstance() = ListViewFragment()
-    }
-
     private lateinit var viewModel: ListViewViewModel
+    private val listNews = mutableListOf<News>()
+    private val adapter = NewsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,22 +34,10 @@ class ListViewFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ListViewViewModel::class.java)
         rec_view.layoutManager = LinearLayoutManager(context)
-        val adapter = NewsAdapter()
         rec_view.adapter = adapter
-
-
         GlobalScope.launch {
             getData()
         }
-//        val list : MutableList<News> = mutableListOf()
-//        list.add(News("title_news1", "description_news1", "http://www.bashinform.ru/upload/img_res1280/ec70f5a6b43bcd52/f_1_jpg_crop1554900657_ejw_1280.jpg"))
-//        list.add(News("title_news2", "description_news2", "http://www.bashinform.ru/upload/img_res1014/57cf7777c2590986/sledkom_jpg_crop1552325137_ejw_1017_jpg_crop1554904872_ejw_1014.jpg"))
-//        list.add(News("title_news3", "description_news3", "http://www.bashinform.ru/upload/img_res1280/5e699a8fb5e443fe/YROV5338_2_jpg_crop1547205705_ejw_1280_jpg_crop1554903171_ejw_1280.jpg"))
-//        list.add(News("title_news4", "description_news4", "http://www.bashinform.ru/upload/img_res1280/d16d72fdfa1e950c/NkRcouKHtsk_jpg_crop1554899778_ejw_1280.jpg"))
-//        list.add(News("title_news5", "description_news5", "http://www.bashinform.ru/upload/img_res1280/6997437d272bd84f/YROV8100_JPG_crop1554703689_ejw_1280_jpg_crop1554898311_ejw_1280.jpg"))
-//
-//        adapter.setList(list)
-
     }
 
     private fun getData() {
@@ -59,25 +45,17 @@ class ListViewFragment : Fragment() {
             val document = Jsoup.connect("http://www.bashinform.ru/goroda_bashkortostana/ufa/").get()
             val regex = Regex(".*ru")
             val baseUrl = regex.find(document.baseUri())?.value
-          //  Log.d(TAG, baseUrl.toString())
             val newsElements = document.select("article[class=img-right]")
-
             for (newsElement in newsElements) {
                 val title = newsElement.select("h2").text()
                 val desription = newsElement.select("div[class=trailer]").text()
-
                 val imgLink = baseUrl + newsElement.select("img").attr("src")
-                Log.d(TAG, imgLink)
+                listNews.add(News(title, desription, imgLink))
             }
-
-            //Log.d(TAG, newsElements.toString())
-//            val titles = document.select("h2")
-//
-//            for (title in titles) {
-//                Log.d(TAG, title.text().toString())
-//            }
+            GlobalScope.launch(Dispatchers.Main) {
+                adapter.setList(listNews)
             }
-        catch (e: Exception) {
+        } catch (e: Exception) {
             Log.e(TAG, "getData: something wrong with get data. Error = $e")
         }
     }
